2436ba233ce153f73c158898dd3e0cabf05fe720
prasoonrajput <prasoonrajput4218@gmail.com>
2024-07-10 20:28:11 +0530

    added the bycryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 const router = express.Router();
